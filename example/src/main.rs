3bd94d75f23e80945d273fe6bad50d01c7fff594
François Mockers <mockersf@gmail.com>
2017-12-21 03:59:22 +0100

    update example

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,6 +2,8 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_dynamodb;
+#[macro_use]
+extern crate serde_dynamodb_derive;
 
 extern crate rusoto_core;
 extern crate rusoto_dynamodb;
@@ -9,30 +11,45 @@ extern crate rusoto_dynamodb;
 extern crate uuid;
 
 use rusoto_core::{DefaultCredentialsProvider, Region};
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput, QueryInput};
 use rusoto_core::default_tls_client;
 
-#[derive(Serialize, Deserialize)]
-struct Todo {
-    id: uuid::Uuid,
-    title: &'static str,
-    done: bool,
+use serde_dynamodb::ToQueryInput;
+
+#[derive(Serialize, Deserialize, ToQueryInput)]
+struct Task {
+    id: String,
+    #[serde(rename = "mystatus")]
+    status: Option<String>,
 }
 
 fn main() {
-    let todo = Todo {
-        id: uuid::Uuid::new_v4(),
-        title: "publish crate",
-        done: false,
+    let provider = DefaultCredentialsProvider::new().unwrap();
+    let client = DynamoDbClient::new(default_tls_client().unwrap(), provider, Region::UsEast1);
+
+    let task = Task {
+        id: String::from("Entry ID"),
+        status: Some(String::from("some status")),
     };
 
-    let put_item = PutItemInput {
-        item: serde_dynamodb::to_hashmap(&todo).unwrap(),
-        table_name: "todos".to_string(),
+    let _query_params = PutItemInput {
+        table_name: String::from("TableName"),
+        item: serde_dynamodb::to_hashmap(&task).unwrap(),
+        ..Default::default()
+    };
+    
+
+    let task_query_input = TaskQueryInput {
+        id: Some("Entry Id".to_string()),
         ..Default::default()
     };
 
-    let provider = DefaultCredentialsProvider::new().unwrap();
-    let client = DynamoDbClient::new(default_tls_client().unwrap(), provider, Region::UsEast1);
-    client.put_item(&put_item).unwrap();
+    let _my_tasks: Vec<Task> = client
+        .query(&task_query_input.to_query_input(String::from("tableName")))
+        .unwrap()
+        .items
+        .unwrap_or_else(|| vec![])
+        .into_iter()
+        .map(|item| serde_dynamodb::from_hashmap(item).unwrap())
+        .collect();
 }
